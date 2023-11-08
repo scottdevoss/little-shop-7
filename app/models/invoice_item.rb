5f4df86fda7d794c181265fe2a1cdd35e08c3a37
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-11-08 12:07:49 -0700

    Small formatting changes, removed pry and save and open pages

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -9,29 +9,10 @@ class InvoiceItem < ApplicationRecord
     self.item.merchant.id
   end
 
-  # def name
-  #   self.item.name
-  # end
-
-  # def status_number
-  #   if self.status == 0
-  #     "pending"
-  #   elsif self.status == 1
-  #     "packaged"
-  #   else
-  #     "shipped"
-  #   end
-  # end
-
   def change_status(new_status)
     self.status = new_status
     self.save
   end
-
-  # def self.alphabetical
-  #   # take all invoice items and sort them by their item names
-  #   InvoiceItem.select("invoice_items.*, items.name").where('invoice_items.item_id = item.id').order('item.name')
-  # end
 
   def self.most_recent
     order(created_at: :desc)

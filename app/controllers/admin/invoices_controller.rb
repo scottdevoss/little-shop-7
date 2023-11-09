8be8cbf6fa2dc0fc87060c3003f0f61181ff13f0
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-11-08 18:26:28 -0700

    revert controller and model back to status of this afternoon

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -1,11 +1,23 @@
 class Admin::InvoicesController < ApplicationController
   def index
-    @invoices = Invoice.all.sorted(params)
+    @invoices = Invoice.all
+    if params[:sort] == "alphabetical"
+      @invoices = Invoice.alphabetical
+    elsif params[:sort] == "date"
+      @invoices = Invoice.most_recent
+    else
+      @invoices = Invoice.all
+    end
   end
 
   def show
     @invoice = Invoice.find(params[:id])
-    @invoice_items = @invoice.sort_invoice_items(params)
+    if params[:sort] == "date"
+      @invoice_items = @invoice.invoice_items.most_recent
+    else
+      @invoice_items = @invoice.invoice_items
+    end
+    # @invoice_items = @invoice.sort_invoice_items(params)
   end
 
   def update
--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,13 @@
 class AdminController < ApplicationController
   def index
     @topfivecustomers = Customer.top_five
-    @invoices = Customer.sort_invoices(params)
+    if params[:sort] == "alphabetical"
+      @invoices = Invoice.alphabetical
+    elsif params[:sort] == "date"
+      @invoices = Invoice.most_recent
+    else
+      @invoices = Invoice.all
+    end
+    # @invoices = Customer.sort_invoices(params)
   end
 end

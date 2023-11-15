6c3cde06962a030c548091d2d0371050475024fc
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-15 11:27:59 -0700

    Edit and Update defined in merchant_discounts controller

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -34,4 +34,19 @@ class MerchantDiscountsController < ApplicationController
     redirect_to "/merchants/#{merchant.id}/discounts"
   end
 
+  def edit 
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = Discount.find(params[:id])
+  end
+
+  def update 
+    discount = Discount.find([params[:id]]) 
+    merchant = Merchant.find([params[:merchant_id]])
+    if params[:new_rate] =~ /\A\d+\Z/  && params[:threshold] =~ /\A\d+\Z/
+      discount.first.update({ discount: params[:new_rate], qty: params[:threshold], merchant_id: params[:merchant_id] })
+    else
+      flash[:notice] = "Discount not added. The discount rate or quantity field was not filled in correctly. Please try again."
+    end
+    redirect_to "/merchants/#{params[:merchant_id]}/discounts/#{params[:id]}"
+  end
 end

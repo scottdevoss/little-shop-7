e4f455c8b38052e58d01f64960515025ff6768b9
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-15 02:20:55 -0700

    @merchant to index b/c index html view needed merchant id for new discount link

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -1,10 +1,28 @@
 class MerchantDiscountsController < ApplicationController 
   def index 
     @discounts = Discount.all
+    @merchant = Merchant.find(params[:merchant_id])
   end
 
   def show 
     @discount = Discount.find(params[:id])
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def new 
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create 
+    merchant = Merchant.find(params[:merchant_id])
+
+    if params[:new_rate] =~ /\A\d+\Z/  && params[:threshold] =~ /\A\d+\Z/ 
+      discount = Discount.new({discount: params[:new_rate], qty: params[:threshold], merchant_id: params[:merchant_id]})
+      discount.save
+    else
+      flash[:notice] = "Discount not added. The discount rate or quantity field was not filled in correctly. Please try again."
+    end
+      redirect_to "/merchants/#{merchant.id}/discounts"
+  end
+
 end

67597ccb108e68cc4581736b765c56aeb75a8694
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-14 21:15:49 -0700

    define index and show in controller for merchant_discounts controller

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -1,5 +1,10 @@
 class MerchantDiscountsController < ApplicationController 
   def index 
-    
+    @discounts = Discount.all
+  end
+
+  def show 
+    @discount = Discount.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
   end
 end

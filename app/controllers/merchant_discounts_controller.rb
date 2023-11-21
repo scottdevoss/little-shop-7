b09be120de9c6b138aa2b3e4dc0be9634da08feb
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-20 19:35:03 -0700

    Remove user input data validation from merchant discounts controller to a numerical/integer validation in the Discount class

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -1,8 +1,7 @@
 class MerchantDiscountsController < ApplicationController 
   def index 
-    @discounts = Discount.all
     @merchant = Merchant.find(params[:merchant_id])
-    
+    @discounts = @merchant.discounts
   end
 
   def show 
@@ -17,13 +16,21 @@ class MerchantDiscountsController < ApplicationController
   def create 
     merchant = Merchant.find(params[:merchant_id])
 
-    if params[:new_rate] =~ /\A\d+\Z/  && params[:threshold] =~ /\A\d+\Z/ 
-      discount = Discount.new({discount: params[:new_rate], qty: params[:threshold], merchant_id: params[:merchant_id]})
-      discount.save
-    else
-      flash[:notice] = "Discount not added. The discount rate or quantity field was not filled in correctly. Please try again."
+    # if params[:new_rate] =~ /\A\d+\Z/  && params[:threshold] =~ /\A\d+\Z/ 
+    #   discount = Discount.new({discount: params[:new_rate], qty: params[:threshold], merchant_id: params[:merchant_id]})
+    #   discount.save
+    # else
+    #   flash[:notice] = "Discount not added. The discount rate or quantity field was not filled in correctly. Please try again."
+    # end
+    #   redirect_to "/merchants/#{merchant.id}/discounts"
+    discount = Discount.new({discount: params[:new_rate], qty: params[:threshold], merchant_id: params[:merchant_id]})
+
+    if discount.save 
+      flash[:notice] = "New discount successfully added."
+    else 
+      flash[:notice] = "Discount not added. Rate or quantity has invalid data. Please try again."
     end
-      redirect_to "/merchants/#{merchant.id}/discounts"
+    redirect_to "/merchants/#{merchant.id}/discounts"
   end
 
   def destroy 
@@ -40,13 +47,15 @@ class MerchantDiscountsController < ApplicationController
   end
 
   def update 
-    discount = Discount.find([params[:id]]) 
-    merchant = Merchant.find([params[:merchant_id]])
-    if params[:new_rate] =~ /\A\d+\Z/  && params[:threshold] =~ /\A\d+\Z/
-      discount.first.update({ discount: params[:new_rate], qty: params[:threshold], merchant_id: params[:merchant_id] })
-    else
-      flash[:notice] = "Discount not added. The discount rate or quantity field was not filled in correctly. Please try again."
-    end
+    discount = Discount.find([params[:id]]).first
+    merchant = Merchant.find([params[:merchant_id]]).first
+
+    if discount.update({ discount: params[:new_rate], qty: params[:threshold], merchant_id: params[:merchant_id] })
+      flash[:notice] = "Discount successfully updated."
+  else
+    flash[:notice] = "Discount not added. Rate or quantity has invalid data. Please try again."
+  end
     redirect_to "/merchants/#{params[:merchant_id]}/discounts/#{params[:id]}"
   end
+
 end

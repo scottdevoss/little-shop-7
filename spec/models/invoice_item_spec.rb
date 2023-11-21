b92fc66c0902c27257c133ae68ccdda1dc533cb7
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-20 19:48:45 -0700

    Add three new bulk discounts to before statement in invoice item spec test.

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -13,6 +13,9 @@ RSpec.describe InvoiceItem, type: :model do
     @invoice_item1 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 1, unit_price: 111, status: 1, created_at: "2012-03-25 09:54:09 UTC") 
     @invoice_item2 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 1, unit_price: 75107, status: 1, created_at: "2012-03-24 09:54:09 UTC") 
     @invoice_item3 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 1, unit_price: 75107, status: 1, created_at: "2012-03-26 09:54:09 UTC") 
+    @discount1 = Discount.create!({discount: 30, qty: 20, merchant_id: @merchant1.id})
+    @discount1 = Discount.create!({discount: 25, qty: 15, merchant_id: @merchant1.id})
+    @discount1 = Discount.create!({discount: 20, qty: 10, merchant_id: @merchant1.id})
     
   end
 
@@ -37,4 +40,30 @@ RSpec.describe InvoiceItem, type: :model do
       expect(InvoiceItem.most_recent).to eq([@invoice_item3, @invoice_item1, @invoice_item2])
     end
   end
+
+  describe "#discount_rate" do 
+    it "returns the highest discount rate for which an item qualifies based on the quantity purchased" do 
+      merchant1 = Merchant.create!(name: "Hannah's Handbags")
+      customer1 = Customer.create!(first_name: "Joey", last_name: "Ondricka")
+      item1 = Item.create!(name: "Item Qui Esse", description: "Nihil autem sit odio inventore deleniti.", unit_price: 75107, merchant_id: merchant1.id)
+      invoice1 = Invoice.create!(status: "completed", customer_id: customer1.id, created_at: "2012-03-25 09:54:09 UTC", updated_at: "2012-03-25 09:54:09 UTC")
+      invoice_item1 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice1.id, quantity: 9, unit_price: 111, status: 1, created_at: "2012-03-25 09:54:09 UTC") 
+      invoice_item2 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice1.id, quantity: 10, unit_price: 75107, status: 1, created_at: "2012-03-24 09:54:09 UTC") 
+      invoice_item3 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice1.id, quantity: 14, unit_price: 75107, status: 1, created_at: "2012-03-26 09:54:09 UTC") 
+      invoice_item4 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice1.id, quantity: 15, unit_price: 75107, status: 1, created_at: "2012-03-26 09:54:09 UTC") 
+      invoice_item5 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice1.id, quantity: 19, unit_price: 75107, status: 1, created_at: "2012-03-26 09:54:09 UTC") 
+      invoice_item6 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice1.id, quantity: 20, unit_price: 75107, status: 1, created_at: "2012-03-26 09:54:09 UTC") 
+      discount1 = Discount.create!({discount: 20, qty: 10, merchant_id: merchant1.id})
+      discount2 = Discount.create!({discount: 25, qty: 15, merchant_id: merchant1.id})
+      discount3 = Discount.create!({discount: 30, qty: 20, merchant_id: merchant1.id})
+      
+      expect(invoice_item1.discount_rate).to eq(0)
+      expect(invoice_item2.discount_rate).to eq(0.2)
+      expect(invoice_item3.discount_rate).to eq(0.2)
+      expect(invoice_item4.discount_rate).to eq(0.25)
+      expect(invoice_item5.discount_rate).to eq(0.25)
+      expect(invoice_item6.discount_rate).to eq(0.3)
+    end
+  end
+
 end

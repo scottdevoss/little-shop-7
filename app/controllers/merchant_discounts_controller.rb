f631db1b890b373dcfc7cebe6864998cf1ea7bb5
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-21 13:47:28 -0700

    Remove old (commented out) regex data validation in merchant discounts controller. Discount class model updated to validate integer input for both dicount % and quantity threshold.

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -15,14 +15,6 @@ class MerchantDiscountsController < ApplicationController
 
   def create 
     merchant = Merchant.find(params[:merchant_id])
-
-    # if params[:new_rate] =~ /\A\d+\Z/  && params[:threshold] =~ /\A\d+\Z/ 
-    #   discount = Discount.new({discount: params[:new_rate], qty: params[:threshold], merchant_id: params[:merchant_id]})
-    #   discount.save
-    # else
-    #   flash[:notice] = "Discount not added. The discount rate or quantity field was not filled in correctly. Please try again."
-    # end
-    #   redirect_to "/merchants/#{merchant.id}/discounts"
     discount = Discount.new({discount: params[:new_rate], qty: params[:threshold], merchant_id: params[:merchant_id]})
 
     if discount.save 
@@ -53,7 +45,7 @@ class MerchantDiscountsController < ApplicationController
     if discount.update({ discount: params[:new_rate], qty: params[:threshold], merchant_id: params[:merchant_id] })
       flash[:notice] = "Discount successfully updated."
   else
-    flash[:notice] = "Discount not added. Rate or quantity has invalid data. Please try again."
+    flash[:notice] = "Discount not updated. Rate or quantity has invalid data. Please try again."
   end
     redirect_to "/merchants/#{params[:merchant_id]}/discounts/#{params[:id]}"
   end

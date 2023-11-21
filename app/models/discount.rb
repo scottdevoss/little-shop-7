2dedb74cd4e3f73025e65568458dfa5fbcd81a33
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-20 19:36:28 -0700

    Add  user input validation - integer datatype for new and updated discount info.

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,3 +1,6 @@
 class Discount < ApplicationRecord
-  belongs_to :merchant
+  belongs_to :merchant 
+  
+  validates :discount, numericality: { only_integer: true }
+  validates :qty, numericality: { only_integer: true }
 end
